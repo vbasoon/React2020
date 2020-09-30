82e3b560c5dc4d5123e277c4c965265348e52551
Басун Володимир <vbasoon@gmail.com>
2020-09-30 10:39:15 +0300

    create component ToDo

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import React from 'react';
-
-import './App.css';
+import React from "react";
+import "./App.css";
+import TodoList from "./components/Todo/TodoList";
 
 function App() {
   return (
     <div className="wrapper">
       <h1>React App</h1>
+      <TodoList />
     </div>
   );
 }
---
+++ src/components/Todo/TodoList.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const TodoList = () => {
+  return (
+    <ul>
+      <li>1</li>
+      <li>2</li>
+    </ul>
+  );
+};
+
+export default TodoList;
