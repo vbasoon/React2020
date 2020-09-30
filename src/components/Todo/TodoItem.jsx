fc8792cf165724e48b56849c5b6b8b50e3603dd1
Басун Володимир <vbasoon@gmail.com>
2020-09-30 16:10:02 +0300

    add styles to remove-button

--- src/components/Todo/TodoItem.jsx
+++ src/components/Todo/TodoItem.jsx
@@ -1,15 +1,30 @@
 import React from "react";
 import PropTypes from 'prop-types'
 
+const styles = {
+  li: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    padding: '0.5rem 1rem',
+    border: '1px solid #ccc',
+    borderRadius: '4px',
+    marginBottom: '0.5rem'
+  },
+  input: {
+    marginRight: '1rem'
+  }
+}
+
 const TodoItem = ({ todo, index }) => {
   return (
-    <li>
+    <li style={styles.li}>
       <span>
-        <input type="checkbox" />
-        <strong>{index + 1}</strong>
-        {todo.title}
+        <input type="checkbox" style={styles.input} />
+        <strong>{index + 1}.</strong>
+        &nbsp;{todo.title}
       </span>
-      <button>&times;</button>
+      <button className="btn remove-button">&times;</button>
 
     </li>
   )
