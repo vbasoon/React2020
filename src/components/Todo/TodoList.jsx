fabb056de050943cf35f37d46ae017bab2f30a58
Басун Володимир <vbasoon@gmail.com>
2020-09-30 10:43:27 +0300

    const styles

--- src/components/Todo/TodoList.jsx
+++ src/components/Todo/TodoList.jsx
@@ -1,8 +1,14 @@
 import React from "react";
 
+const styles = {
+  ul: {
+    listStyle: "none",
+  },
+};
+
 const TodoList = () => {
   return (
-    <ul>
+    <ul style={styles.ul}>
       <li>1</li>
       <li>2</li>
     </ul>
